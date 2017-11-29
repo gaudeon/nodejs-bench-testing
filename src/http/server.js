1430c18384e8df0b766e3351c06114b9edef958d
Travis Chase <travis.chase@gmail.com>
2017-11-29 12:24:29 -0700

    Added Connection close header

--- src/http/server.js
+++ src/http/server.js
@@ -3,16 +3,17 @@ const http = require('http');
 const port = process.argv.length > 2 && process.argv[2].match(/^\d+$/) && process.argv[2] * 1 > 1024 ? process.argv[2] : 8080;
 
 const requestHandler = (request, response) => {
-  //console.log(request.url);
-  response.end('Hello\r\n');
+    //console.log(request.url);
+    response.setHeader('Connection', 'close');
+    response.end('Hello\r\n');
 };
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err);
-  }
+    if (err) {
+        return console.log('something bad happened', err);
+    }
 
-  //console.log(`server is listening on ${port}`);
+    //console.log(`server is listening on ${port}`);
 });
